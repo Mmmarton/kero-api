f9df24d9fec23fa27ceac1d1369ead87ce70016e
Marton <warcodhiro@gmail.com>
2018-02-06 13:39:43 +0200

    changed salting mechanism

--- src/main/java/com/komak/kero/keroapi/user/UserService.java
+++ src/main/java/com/komak/kero/keroapi/user/UserService.java
@@ -2,16 +2,12 @@ package com.komak.kero.keroapi.user;
 
 import com.komak.kero.keroapi.auth.Credentials;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
-    @Value("${encoder.salt}")
-    private String salt;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -23,7 +19,7 @@ public class UserService {
     private UserRepository userRepository;
 
     public User getByCredentials(Credentials credentials) {
-        User user = userRepository.findByUsername(usernameEncoder.encodePassword(credentials.getUsername(), salt));
+        User user = userRepository.findByUsername(usernameEncoder.encodePassword(credentials.getUsername(), credentials.getPassword()));
         if (user != null && passwordEncoder.matches(credentials.getPassword(), user.getPassword())) {
             return user;
         }
@@ -31,7 +27,7 @@ public class UserService {
     }
 
     public void create(User user) {
-        user.setUsername(usernameEncoder.encodePassword(user.getUsername(), salt));
+        user.setUsername(usernameEncoder.encodePassword(user.getUsername(), user.getPassword()));
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
