b128abc4412d25da3ae6600cd76cbdee4d08a1a0
Marton <warcodhiro@gmail.com>
2018-02-22 21:35:03 +0200

    fix messed up commit

--- src/main/java/com/komak/kero/keroapi/user/UserController.java
+++ src/main/java/com/komak/kero/keroapi/user/UserController.java
@@ -6,6 +6,7 @@ import com.komak.kero.keroapi.auth.UserSession;
 import com.komak.kero.keroapi.user.model.UserCreateModel;
 import com.komak.kero.keroapi.user.model.UserDeleteModel;
 import com.komak.kero.keroapi.user.model.UserInviteModel;
+import com.komak.kero.keroapi.user.model.UserRoleModel;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -50,6 +51,18 @@ class UserController {
     UserSession session = authService.getSession();
     if (session.getRole() == Role.ROLE_ADMIN) {
       return new ResponseEntity(UserAdapter.toUserListModel(userService.list()), HttpStatus.OK);
+    }
+    else {
+      return new ResponseEntity("Not authorised.", HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  @RequestMapping(value = "/", method = RequestMethod.PUT)
+  public ResponseEntity<Object> changeRole(@RequestBody @Valid UserRoleModel userRole) {
+    UserSession session = authService.getSession();
+    if (session.getRole() == Role.ROLE_ADMIN) {
+      userService.changeRole(userRole);
+      return new ResponseEntity("Done.", HttpStatus.OK);
     }
     else {
       return new ResponseEntity("Not authorised.", HttpStatus.UNAUTHORIZED);
